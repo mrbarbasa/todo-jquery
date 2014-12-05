0b8c4ecd3924045f68f45ccffa60669f164c3d29
Marifel <mrbarbasa@gmail.com>
2014-12-04 19:52:40 -1000

    Finish Level 2

--- public/js/todo.js
+++ public/js/todo.js
@@ -7,9 +7,28 @@ $(function() {
       input.val(""); // Clear out the text after Enter is pressed
 
       var todoItem = $("<li>", {
-        html: todoText
+        class: "todo_item",
+        html: $("<input>", {
+          type: "checkbox"
+        })
       });
+      todoItem.append(todoText);
       $("#todos").append(todoItem);
     }
-  })
+  });
+
+  // On click or on change works
+  $("#todos").on("change", ".todo_item input[type=checkbox]", function() {
+    var listItem = $(this).parent();
+
+    // if ($(this).is(":checked")) { // Alternative
+    if (this.checked) { // If user checked
+      listItem.addClass("todo_item_completed");
+    }
+    else { // If user unchecked
+      if (listItem.hasClass("todo_item_completed")) {
+        listItem.removeClass("todo_item_completed");
+      }
+    }
+  });
 });
