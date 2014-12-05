d1b7e3c39a2f5a85f14922d3b2abc9672bfbfaf0
Marifel <mrbarbasa@gmail.com>
2014-12-04 19:16:09 -1000

    Finish Level 1

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,3 +1,15 @@
 $(function() {
 
+  $("#todo_input").keypress(function(e) {
+    if (e.which === 13) { // When Enter key is pressed, process the input
+      var input = $(this);
+      var todoText = input.val(); // Save the value before clearing it
+      input.val(""); // Clear out the text after Enter is pressed
+
+      var todoItem = $("<li>", {
+        html: todoText
+      });
+      $("#todos").append(todoItem);
+    }
+  })
 });
